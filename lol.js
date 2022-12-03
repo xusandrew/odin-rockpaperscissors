1f284108fc7dbc9a9d1aca84be046c34062a2baa
Andrew Xu <xusandrew@gmail.com>
2022-12-03 17:04:16 -0500

    Create playRound function

--- lol.js
+++ lol.js
@@ -1,7 +1,21 @@
 function getComputerChoice(){
     var choice = Math.floor(Math.random()*3) + 1
 
-    if (choice === 1) return "Rock";
-    if (choice === 2) return "Paper";
-    if (choice === 3) return "Scissors";
+    if (choice === 1) return "rock";
+    if (choice === 2) return "paper";
+    if (choice === 3) return "scissors";
+}
+
+function playRound (playerSelection, computerSelection){
+    playerSelection = playerSelection.toLowerCase()
+    computerSelection = computerSelection.toLowerCase()
+    if (playerSelection === computerSelection)
+        return "Its a tie!"
+    else if ((playerSelection === "rock" && computerSelection === "scissors") ||
+            (playerSelection === "paper" && computerSelection === "rock") ||
+            (playerSelection === "scissors" && computerSelection === "paper")
+            )
+        return "You win!, " + playerSelection + " beats " + computerSelection
+    else 
+        return "You lose! " + computerSelection + " beats " + playerSelection
 }
